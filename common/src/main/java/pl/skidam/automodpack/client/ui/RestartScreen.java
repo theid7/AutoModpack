55ac0b09dc1518407e9322f80fa02ad2fc33f8df
skidam <skidamgamer@gmail.com>
2023-05-13 21:11:22 +0200

    menu almost

--- common/src/main/java/pl/skidam/automodpack/client/ui/RestartScreen.java
+++ common/src/main/java/pl/skidam/automodpack/client/ui/RestartScreen.java
@@ -16,12 +16,10 @@ public class RestartScreen extends Screen {
     private static ButtonWidget cancelButton;
     private static ButtonWidget restartButton;
     private static ButtonWidget changelogsButton;
-    private final Screen parent;
 
-    public RestartScreen(Screen parent, File gameDir) {
+    public RestartScreen(File gameDir) {
         super(TextHelper.literal("RestartScreen"));
         this.gameDir = gameDir;
-        this.parent = parent;
 
         if (AudioManager.isMusicPlaying()) {
             AudioManager.stopMusic();
@@ -44,23 +42,27 @@ public class RestartScreen extends Screen {
     }
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         this.renderBackground(matrices);
-        drawCenteredText(matrices, this.textRenderer, TextHelper.literal("Restart").formatted(Formatting.BOLD), this.width / 2, 55, 16777215);
-        drawCenteredText(matrices, this.textRenderer, TextHelper.translatable("gui.automodpack.screen.restart.description"), this.width / 2, 80, 16777215);
-        drawCenteredText(matrices, this.textRenderer, TextHelper.translatable("gui.automodpack.screen.restart.secDescription"), this.width / 2, 90, 16777215);
+        drawCenteredTextWithShadow(matrices, this.textRenderer, TextHelper.translatable("gui.automodpack.restart").formatted(Formatting.BOLD), this.width / 2, 55, 16777215);
+        drawCenteredTextWithShadow(matrices, this.textRenderer, TextHelper.translatable("gui.automodpack.restart.description"), this.width / 2, 80, 16777215);
+        drawCenteredTextWithShadow(matrices, this.textRenderer, TextHelper.translatable("gui.automodpack.restart.secDescription"), this.width / 2, 90, 16777215);
         super.render(matrices, mouseX, mouseY, delta);
     }
+
     public void initWidgets() {
         assert this.client != null;
-        cancelButton = new ButtonWidget(this.width / 2 - 150, this.height / 6 + 120, 150, 20, TextHelper.translatable("gui.automodpack.screen.restart.button.cancel").formatted(Formatting.RED), (button) -> {
+        cancelButton = ButtonWidget.builder(TextHelper.translatable("gui.automodpack.restart.cancel").formatted(Formatting.RED), button -> {
             this.client.setScreen(null);
-        });
-        restartButton = new ButtonWidget(this.width / 2, this.height / 6 + 120, 150, 20, TextHelper.translatable("gui.automodpack.screen.restart.button.quit").formatted(Formatting.GREEN), (button) -> {
+        }).position(this.width / 2 - 150, this.height / 6 + 120).size(150, 20).build();
+
+        restartButton = ButtonWidget.builder(TextHelper.translatable("gui.automodpack.restart.quit").formatted(Formatting.GREEN), button -> {
             new ReLauncher.Restart(gameDir);
-        });
-        changelogsButton = new ButtonWidget(this.width / 2 - 75, this.height / 6 + 145, 150, 20, TextHelper.translatable("gui.automodpack.screen.restart.button.changelogs").formatted(Formatting.DARK_AQUA), (button) -> {
+        }).position(this.width / 2, this.height / 6 + 120).size(150, 20).build();
+
+        changelogsButton = ButtonWidget.builder(TextHelper.translatable("gui.automodpack.restart.changelogs").formatted(Formatting.DARK_AQUA), button -> {
             this.client.setScreen(new ChangelogScreen(this, gameDir));
-        });
+        }).position(this.width / 2 - 75, this.height / 6 + 145).size(150, 20).build();
     }
+
     @Override
     public boolean shouldCloseOnEsc() {
         return false;
